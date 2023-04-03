ba21c8c2fb8fda32e72d8a3541fc80102184eb50
Jared Enck <jaredenck86@gmail.com>
2023-04-03 14:59:23 -0400

    update

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ const PRODUCTION_URI = 'postgresql:///jobly'.replace('://', 'ql://', 1)
 
 if (process.env.NODE_ENV === "production") {
   db = new Client({
-    connectionString: DB_URI || PRODUCTION_URI,
+    connectionString: PRODUCTION_URI,
     ssl: process.env.DATABASE_URL ? true : false
   });
 } else {
