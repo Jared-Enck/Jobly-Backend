2097d2ca4ea857598090cecae04721aa65bd7c83
Jared Enck <jaredenck86@gmail.com>
2023-04-03 14:57:57 -0400

    update

--- db.js
+++ db.js
@@ -2,19 +2,16 @@
 /** Database setup for jobly. */
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
-import os from 'os'
 
 let db;
 
 const DB_URI = `socket:/var/run/postgresql?db=${getDatabaseUri()}`
-const PRODUCTION_URI = os.environ.get('DATABASE_URL', 'postgresql:///jobly').replace('://', 'ql://', 1)
+const PRODUCTION_URI = 'postgresql:///jobly'.replace('://', 'ql://', 1)
 
 if (process.env.NODE_ENV === "production") {
   db = new Client({
     connectionString: DB_URI || PRODUCTION_URI,
-    ssl: {
-      rejectUnauthorized: false
-    }
+    ssl: process.env.DATABASE_URL ? true : false
   });
 } else {
   db = new Client({
