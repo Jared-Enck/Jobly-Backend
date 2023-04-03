973f84345688604c4d8f6cfd32ccd5b542dc033b
Jared Enck <jaredenck86@gmail.com>
2023-04-03 15:20:53 -0400

    update

--- db.js
+++ db.js
@@ -10,9 +10,9 @@ const DB_URI = `socket:/var/run/postgresql?db=${getDatabaseUri()}`
 if (process.env.NODE_ENV === "production") {
   db = new Client({
     connectionString: DB_URI,
-    ssl: {
-      rejectUnauthorized: false
-    }
+    // ssl: {
+    //   rejectUnauthorized: false
+    // }
   });
 } else {
   db = new Client({
